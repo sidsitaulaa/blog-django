6519da7653516f37e9b11e9f4bad0b350a9dc3ba
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-22 17:46:52 +0545

    [ADD] posts editing feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,7 +8,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html',{'posts':posts})
 
 
-def post_default(request,id):
+def post_detail(request,id):
     post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
 
     return render(request,'blog/post/detail.html',{'post':post})
