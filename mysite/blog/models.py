4b9762378a72ecb8aa7d3f29da3cf1f186517610
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-22 16:17:04 +0545

    [ADD] initial setup

---
+++ mysite/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+
+class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
+    title= models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF','DRAFT'
+        PUBLISHED = 'PB', 'Published'
+
+    status=models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    
+    class Meta:
+        ordering=['-publish']
+        indexes=[
+            models.Index(fields=['-publish'])
+        ]
+    def __str__(self):
+        return self.title
