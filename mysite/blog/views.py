8b01f0ed96aa48b560e9b2ea657ef0f92d7543fb
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-23 16:02:32 +0545

    [ADD] added commenting feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_share(request,post_id):
@@ -45,7 +46,9 @@ def post_list(request):
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post':post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
 
 
 class PostListView(ListView):
@@ -53,5 +56,23 @@ class PostListView(ListView):
     context_object_name='posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request,'blog/post/comment.html',{
+        'post':post,
+        'form':form,
+        'comment':comment
+    })
 
 
